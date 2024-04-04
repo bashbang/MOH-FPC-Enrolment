42e4777178f5f7709ebea6a5d76b4dd4e89aa258
Cyril Bravo <Cyril.Bravo@hibc.gov.bc.ca>
2024-04-04 08:39:22 -0700

    chore: commit version file

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,10 +1,10 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '1b4ce8b';
-export const buildTime = '9/23/2022 at 4:56:55 PM';
+export const gitCommit = '2424faf';
+export const buildTime = '4/4/2024 at 8:39:22 AM';
 /** App version retrieved from package.json. */
-export const projectVersion = '4.2.2';
+export const projectVersion = '4.2.3';
 /** Human readable message  */
-export const message = 'Canonical Version: 1b4ce8b - 9/23/2022 at 4:56:55 PM (v4.2.2) ' +
-'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/1b4ce8b';
+export const message = 'Canonical Version: 2424faf - 4/4/2024 at 8:39:22 AM (v4.2.3) ' +
+'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/2424faf';
 /** If true, other values should be present. */
 export const success = true;
